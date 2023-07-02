d9aeb955a6387133a61c707e93ba1afa6c3d2105
Nenad Stojchevski <nenad.stojchevski@deptagency.com>
2023-07-02 10:52:10 +0200

    Added home component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-
-class ShoppingList extends React.Component{
-
-    render(){
-
-
-    }
-
-
-
-
-}
-export default ShoppingList;
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
