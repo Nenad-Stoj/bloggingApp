812accff8686ac2ce54ad88e80be0d8155cb3ca6
Nenad Stojchevski <nenad.stojchevski@deptagency.com>
2023-07-08 16:51:05 +0200

    added error 404 and updated details and create page

--- src/Create.js
+++ src/Create.js
@@ -1,11 +1,62 @@
-const Create = () => {
-    return (  
+import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
-        <div className="create"> 
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('name1')
+    const [isPending, setIsPending] = useState(false)
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+        console.log(blog);
+        setIsPending(true);
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("New blog added to Database")
+            setIsPending(false);
+            history.push("/")
+        })
         
-        <h2>Add a New Blog</h2>
+    }
+
+    return (
+
+        <div className="create">
+
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                ></input>
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="name1">Name 1</option>
+                    <option value="name2">Name 2</option>
+                </select>
+                {!isPending && <button>Add blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
+            </form>
         </div>
     );
 }
- 
+
 export default Create;
